b61b1179973d05a05a1ccd4331b4f6e1852a3262
Michele Curtaz <mcurtaz@gmail.com>
2020-06-17 17:08:11 +0200

    calcolo preventivo js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var costoKm = 0.21;
 // serie di istruzioni sul click su crea biglietto
 
 document.getElementById("btnCrea").addEventListener("click", function(){
+
   // creo variabili con dati dal form
 
   var nomeCompleto = document.getElementById("nome").value + " " + document.getElementById("cognome").value;
@@ -18,4 +19,19 @@ document.getElementById("btnCrea").addEventListener("click", function(){
   var riduzione = document.getElementById("riduzione").value;
 
   console.log(riduzione);
+
+  // calcolo prezzo biglietto
+  if (riduzione == "minori") {
+    // se l'utente ha selezionato minori di 18 il prezzo è totale per 0.8 cioè l'80% del totale (sconto 20%)
+    var prezzoBiglietto = km * costoKm * 0.8;
+  } else if (riduzione == "senior"){
+    // se l'utente ha selezionato senior il prezzo è totale per 0.6 cioè il 60% del totale (sconto 40%)
+    var prezzoBiglietto = km * costoKm * 0.6;
+  } else {
+    // se non c'è riduzione il prezzo è semplicemente km * costo al kilometro
+    var prezzoBiglietto = km * costoKm;
+  }
+
+  console.log(prezzoBiglietto);
+
 });
